047526b7240c2b784a3a922330aa89192eb34e6e
Klavbe <150854735+OJKulka@users.noreply.github.com>
2026-06-19 16:30:01 +0200

    Delete Archiw directory

--- Archiw/frontend/src/components/ui/label.jsx
+++
@@ -1,7 +0,0 @@
-export const Label = ({ children, className = "", ...props }) => {
-  return (
-    <label className={className} {...props}>
-      {children}
-    </label>
-  );
-};
--- Archiw/frontend/src/components/ui/sonner.jsx
+++
@@ -1,5 +0,0 @@
-import { Toaster as Sonner } from "sonner";
-
-export const Toaster = (props) => {
-  return <Sonner {...props} />;
-};
