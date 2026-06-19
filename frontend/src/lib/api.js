65fa4feed2a42302d576f334d05f6e45aef930f6
Klavbe <150854735+OJKulka@users.noreply.github.com>
2026-06-19 18:27:08 +0200

    Refactor createCheckout to remove originUrl parameter
    Removed originUrl parameter from createCheckout.

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -82,10 +82,9 @@ export const fetchFilters = async () => {
   }
 };
 
-export const createCheckout = async (items, originUrl) => {
+export const createCheckout = async (items) => {
   const { data } = await api.post("/checkout/session", {
     items,
-    origin_url: originUrl,
   });
 
   return data;
